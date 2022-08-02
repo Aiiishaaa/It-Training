eb804dcbb82c1722f1cefde6756284e60dfd13c8
cilH <heuillet.cecile@gmail.com>
2022-08-02 15:35:56 +0200

    cilH front recup theme sur details formation

--- it-training/src/main/java/com/ittraining/main/dao/ThemeRepository.java
+++ it-training/src/main/java/com/ittraining/main/dao/ThemeRepository.java
@@ -5,4 +5,5 @@ import com.ittraining.main.models.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ThemeRepository extends JpaRepository<Theme, Integer> {
+	Theme findByFormationsId(Integer idFormation);
 }
