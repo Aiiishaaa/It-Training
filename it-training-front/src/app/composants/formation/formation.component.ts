4019817284ea9a4c4fc79ff562e8897bf7ec9cc3
cilH <heuillet.cecile@gmail.com>
2022-08-02 15:34:46 +0200

    cilH front recup theme sur details formation

--- it-training-front/src/app/composants/formation/formation.component.ts
+++ it-training-front/src/app/composants/formation/formation.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Domaine } from 'src/app/interfaces/domaine';
 import { Employe } from 'src/app/interfaces/employe';
 import { Formation } from 'src/app/interfaces/formation';
 import { Prerequis } from 'src/app/interfaces/prerequis';
 import { Session } from 'src/app/interfaces/session';
+import { Theme } from 'src/app/interfaces/theme';
 import { EmployeService } from 'src/app/services/employe.service';
 import { FormationService } from 'src/app/services/formation.service';
 import { SessionService } from 'src/app/services/session.service';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-formation',
@@ -19,6 +22,8 @@ export class FormationComponent implements OnInit {
   formation: Formation = {};
   sessions: Session[] = [];
   contact: Employe = {};
+  theme: Theme = {};
+  domaine: Domaine = {};
   prerequis: Prerequis = {};
   id: number = 0;
 
@@ -26,6 +31,7 @@ export class FormationComponent implements OnInit {
     private formationServ: FormationService,
     private sessionServ: SessionService,
     private employeServ: EmployeService,
+    private themeServ: ThemeService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -34,6 +40,7 @@ export class FormationComponent implements OnInit {
       this.recupFormation(this.id);
       this.recupSessionsParFormation(this.id);
       this.recupContactParFormation(this.id);
+      this.recupThemeParFormation(this.id);
     });
   }
 
@@ -52,6 +59,13 @@ export class FormationComponent implements OnInit {
   recupContactParFormation(idFormation: number) {
     this.employeServ.getOneEmployeByFormation(idFormation).subscribe(res => {
       this.contact = res;
+    })
+  }
+
+  recupThemeParFormation(idFormation: number) {
+    this.themeServ.getOneThemeByFormation(idFormation).subscribe(res => {
+      this.theme = res;
+      console.log(this.theme);
     })
   }
 }
